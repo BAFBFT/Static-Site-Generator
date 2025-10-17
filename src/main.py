29e6da3b93c49fa63deb3ffca5405e5196b1e646
BAFBFT <oluwabukunmi.ayilola@gmail.com>
2025-10-17 23:07:19 +0100

    added texttype and textnode class

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    my_textnode  = TextNode("This is some anchor text", TextType.link, "https://www.boot.dev")
+    print(my_textnode)
+
+if __name__=="__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,22 @@
+from enum import Enum 
+
+class TextType(Enum):
+    text = ""
+    bold = "**Bold text**"
+    italic = "_Italic text_"
+    codetext = "`Code text`"
+    link = "[anchor text](url)"
+    images = "![alt text](url)"
+
+class TextNode:
+    def __init__(self, text, text_type: TextType, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, other):
+        return self.text==other.text and self.text_type==other.text_type and self.url==other.url
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.text}, {self.text_type}, {self.url})"
+    
