fee2a7164c2d09dfb8327fd087a831e2d5b8f58c
BAFBFT <oluwabukunmi.ayilola@gmail.com>
2025-10-17 23:32:27 +0100

    update enum definition

--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,12 @@
 from enum import Enum 
 
 class TextType(Enum):
-    text = ""
-    bold = "**Bold text**"
-    italic = "_Italic text_"
-    codetext = "`Code text`"
-    link = "[anchor text](url)"
-    images = "![alt text](url)"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
     def __init__(self, text, text_type: TextType, url=None):
@@ -15,8 +15,12 @@ class TextNode:
         self.url = url
     
     def __eq__(self, other):
-        return self.text==other.text and self.text_type==other.text_type and self.url==other.url
+        return (
+            self.text==other.text 
+            and self.text_type==other.text_type 
+            and self.url==other.url
+        )
 
     def __repr__(self):
-        return f"{type(self).__name__}({self.text}, {self.text_type}, {self.url})"
+        return f"{type(self).__name__}({self.text}, {self.text_type.value}, {self.url})"
     
