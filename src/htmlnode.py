74820f6665312c8d0cf94a27876645f9352b8b66
BAFBFT <oluwabukunmi.ayilola@gmail.com>
2026-08-15 18:48:48 +0100

    added leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,6 +1,12 @@
 
 class HTMLNode:
-    def __init__(self, tag=None, value=None, children=list|None, props=dict|None):
+    def __init__(
+        self,
+        tag: str | None = None,
+        value: str | None = None,
+        children: list["HTMLNode"] | None = None,
+        props: dict[str, str] | None = None,
+    ):
         self.tag = tag
         self.value = value
         self.children = children
@@ -21,3 +27,23 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(
+            self, 
+            tag: str | None, 
+            value : str, 
+            props: dict[str, str] | None = None):
+        super().__init__(tag, value, None, props)
+        
+    def to_html(self):
+        if not self.value:
+            raise ValueError("all leaf nodes must have a value.")
+        if not self.tag:
+            return self.value
+        html_props = super().props_to_html()
+        return f"<{self.tag}{html_props}>{self.value}</{self.tag}>"
+    
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self.tag}, {self.value}, {self.props})"
