2f1c8606ebad3479911a48cef4f998c1c2cb941d
BAFBFT <oluwabukunmi.ayilola@gmail.com>
2026-08-16 09:40:24 +0100

    added tests for leaf nodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -18,7 +18,7 @@ class TestTextNode(unittest.TestCase):
 
     def test_props_to_html_string_match(self):
         # test leading and trailing whitespace
-        node = HTMLNode("p", "yo, I like your cut G", children=None, 
+        node = HTMLNode("p", "yo, I like your cut s", children=None, 
                         props={
                                 "href": "https://www.google.com",
                             }
@@ -36,6 +36,19 @@ class TestTextNode(unittest.TestCase):
                         )
         html_repr =' href="https://www.google.com" target="_blank"'
         self.assertEqual(node.props_to_html(), html_repr)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_repr(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.__repr__(), "LeafNode(p, Hello, world!, None)")
+
+    def test_leaf_children_is_none(self):
+        node = LeafNode("p", "hello")
+        self.assertIsNone(node.children)
+        
 
 if __name__ == "__main__":
     unittest.main()
