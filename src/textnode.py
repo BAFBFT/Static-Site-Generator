a97bd51d255e4699adede2bf82dd0adb364e73d1
BAFBFT <oluwabukunmi.ayilola@gmail.com>
2026-08-16 11:38:42 +0100

    added textnode to html conversion

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -9,8 +11,9 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
+
 class TextNode:
-    def __init__(self, text, text_type: TextType, url=None):
+    def __init__(self, text, text_type: TextType, url: str = ""):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -24,4 +27,24 @@ class TextNode:
 
     def __repr__(self):
         return f"{type(self).__name__}({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if not isinstance(text_node.text_type, TextType):
+        raise TypeError(f"invalid text type: {text_node.text_type}")
     
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(value=text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            if text_node.url is None:
+                raise ValueError("invalid URL")
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
