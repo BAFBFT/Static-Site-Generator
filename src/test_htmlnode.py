56661bf7aae6b11f2829d2b49f3085d1a3ff7bb1
BAFBFT <oluwabukunmi.ayilola@gmail.com>
2026-08-16 10:32:27 +0100

    added parent node tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -10,11 +10,13 @@ class TestTextNode(unittest.TestCase):
         node_repr = "HTMLNode(<p>, yo, I like your cut G, None, None)"
         self.assertEqual(node.__repr__(), node_repr)
 
+
     def test_props_to_html_no_props(self):
         # test empty props
         node = HTMLNode("p", "yo, I like your cut G", children=None, props=None)
         html_repr =''
         self.assertEqual(node.props_to_html(), html_repr)
+
 
     def test_props_to_html_string_match(self):
         # test leading and trailing whitespace
@@ -25,6 +27,7 @@ class TestTextNode(unittest.TestCase):
                         )
         html_repr =' href="https://www.google.com"'
         self.assertEqual(node.props_to_html(), html_repr)
+
 
     def test_props_to_html_insertion_order(self):
         # test insertion order
@@ -37,18 +40,67 @@ class TestTextNode(unittest.TestCase):
         html_repr =' href="https://www.google.com" target="_blank"'
         self.assertEqual(node.props_to_html(), html_repr)
 
+
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
 
     def test_leaf_repr(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.__repr__(), "LeafNode(p, Hello, world!, None)")
 
+
     def test_leaf_children_is_none(self):
         node = LeafNode("p", "hello")
         self.assertIsNone(node.children)
-        
+
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+
+    def test_to_html_with_multiple_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(node.to_html(),
+                        "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
+
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )        
+
+
+    def test_parent_no_children(self):
+        parent_node = ParentNode("div", [])
+        self.assertRaises(
+            ValueError,
+            parent_node.to_html,
+        )        
+    
+
+    def test_parent_no_tag(self):
+        child_node = LeafNode("b", "grandchild")
+        parent_node = ParentNode("", [child_node])
+        self.assertRaises(
+            ValueError,
+            parent_node.to_html,
+        ) 
 
 if __name__ == "__main__":
     unittest.main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,14 +1,17 @@
 import unittest
+
 from textnode import TextNode, TextType
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
+        # equality tests between 2 nodes
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
     def test_eq_false(self):
+        # inequality tests between 2 nodes
         node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
