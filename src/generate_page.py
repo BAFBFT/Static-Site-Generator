143ba37c5be89ef370456b8c3ca7cf17506befce
BAFBFT <oluwabukunmi.ayilola@gmail.com>
2026-09-07 01:00:21 +0100

    base path added for deployment

--- src/generate_page.py
+++ src/generate_page.py
@@ -13,7 +13,7 @@ def extract_title(markdown: str) -> str:
     raise ValueError("no h1 heading found")
 
 
-def generate_page(from_path: Path, template_path: Path, dest_path: Path) -> None:
+def generate_page(from_path: Path, template_path: Path, dest_path: Path, base_path: Path) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     from_path_str = from_path.read_text(encoding="utf-8")
@@ -26,18 +26,22 @@ def generate_page(from_path: Path, template_path: Path, dest_path: Path) -> None
     template_path_str = template_path_str.replace("{{ Title }}", page_title)
     template_path_str = template_path_str.replace("{{ Content }}", from_path_html)
 
+    # replace with correct basepath
+    template_path_str = template_path_str.replace('href="/', f'href="{base_path}')
+    template_path_str = template_path_str.replace('src="/', f'src="{base_path}')
+
     index_path = dest_path / f"{from_path.stem}.html"
     index_path.parent.mkdir(parents=True, exist_ok=True)
     index_path.write_text(template_path_str, encoding="utf-8")
 
 
-def generate_pages_recursive(from_path: Path, template_path: Path, dest_path: Path) -> None:
+def generate_pages_recursive(from_path: Path, template_path: Path, dest_path: Path,  base_path: Path) -> None:
     print(f"Generating pages from {from_path} to {dest_path} using {template_path}")
 
     for child in from_path.iterdir():
         if child.is_file() and child.suffix == '.md':
-            generate_page(child, template_path, dest_path)
+            generate_page(child, template_path, dest_path, base_path)
         elif child.is_dir():
-            generate_pages_recursive(child, template_path, dest_path / child.name)
+            generate_pages_recursive(child, template_path, dest_path / child.name, base_path)
         else:
             continue
